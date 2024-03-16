ea1eb023123870b851fcb1ee75561e30a58c0d7d
aaditgarg17 <aaditgarg17@gmail.com>
2024-03-16 23:23:23 +0530

    +version 1

---
+++ app.py
@@ -0,0 +1,14 @@
+import flask
+from flask import render_template, url_for
+from poems import poem_dic
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=poem_dic)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
